c834991e48cd9bd8fa88d1e2367eaa541fa995da
Jeffrey-Anto <jeffreyjat@gmail.com>
2025-09-23 21:46:14 +0530

    Element selector added

---
+++ weather.js
@@ -0,0 +1,7 @@
+const btn= document.querySelector(".search-btn");
+const input= document.querySelector("input");
+const show= document.querySelector(".display");
+let temp= document.querySelector(".temp");
+let city = document.querySelector(".city");
+let humidity= document.querySelector(".humi");
+let windspd = document.querySelector(".speed");
