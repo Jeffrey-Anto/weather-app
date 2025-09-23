8b88d09a335bcc9a78b600b385a2fdc4b4ace9bc
Jeffrey-Anto <jeffreyjat@gmail.com>
2025-09-23 22:54:11 +0530

    Data are fetched and displayed

--- weather.js
+++ weather.js
@@ -8,3 +8,19 @@ let windspd = document.querySelector(".speed");
 const apiurl= "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=";
 const apiKey= "40a54fd2dddd5d540da018c9372ce9b4";
 
+btn.addEventListener("click", async function(params) {
+    if(input.value.length==0){
+        alert("Please Enter the city name");
+    } else{
+        show.style.display = 'block';
+        let cityname=input.value;
+        const response= await fetch(apiurl+apiKey+`&q=${cityname}`);
+        const data= await response.json();
+        temp.innerHTML=Math.round(data.main.temp)+"°c";
+        city.innerHTML=data.name;
+        humidity.textContent=data.main.humidity+"%";
+        windspd.innerHTML=data.win.speed +"km/h";
+        let weather=data.weather[0].main;
+    }
+    
+})
