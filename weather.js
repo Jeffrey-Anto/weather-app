e7e798ce36a1b97b6c22da98fca38fae8888ebb7
Jeffrey-Anto <jeffreyjat@gmail.com>
2025-09-23 21:50:43 +0530

    API url and key added

--- weather.js
+++ weather.js
@@ -5,3 +5,6 @@ let temp= document.querySelector(".temp");
 let city = document.querySelector(".city");
 let humidity= document.querySelector(".humi");
 let windspd = document.querySelector(".speed");
+const apiurl= "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=";
+const apiKey= "40a54fd2dddd5d540da018c9372ce9b4";
+
