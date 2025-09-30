574d8acde6257202bea73b7943fd167618af9544
Jeffrey-Anto <jeffreyjat@gmail.com>
2025-09-30 15:06:17 +0530

    Location button and geolocation api added

--- weather.js
+++ weather.js
@@ -1,12 +1,16 @@
 const btn= document.querySelector(".search-btn");
+const loc=document.querySelector(".location-btn");
 const input= document.querySelector("input");
 const show= document.querySelector(".display");
 let temp= document.querySelector(".temp");
 let city = document.querySelector(".city");
 let humidity= document.querySelector(".humi");
 let windspd = document.querySelector(".speed");
+
 const apiurl= "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=";
 const apiKey= "40a54fd2dddd5d540da018c9372ce9b4";
+const geoapiurl="https://ipinfo.io/json?token=";
+const geoapikey="dbfbd0968fe7af";
 
 btn.addEventListener("click", async function(params) {
     if(input.value.length==0){
